a6918c1c6b99e433f8b6c794827d78514f23f75c
abovet <audrey.bovet@ensae.fr>
2026-01-15 09:28:51 +0000

    gestion des colonnes

--- main.py
+++ main.py
@@ -27,7 +27,21 @@ else:
 df = pd.read_csv(path, sep = ";")
 print(df.info())
 # Which kind of visualization do you want?
+# en fonction de la réponse demander les colonnes d'intéret :
+# - date, - une ou deux variables autres, plusieurs variables en fonction du temps ?
 
+# cas 1 date 1 variable
+date = input("What is your date column name? mettre les spec de typo")
+column1 = input("What is your column of interest ?")
 
+#cas juste une variabel
+column1 = input("What is your column of interest ?")
 
-columns = input("What ")
+# cas 1 date 2 variables
+date = input("What is your date column name? mettre les spec de typo")
+column1 = input("What is your column 1 of interest ?")
+column2 = input("What is your column 2 of interest ?")
+
+# cas 2 variables
+column1 = input("What is your column 1 of interest ?")
+column2 = input("What is your column 2 of interest ?")
