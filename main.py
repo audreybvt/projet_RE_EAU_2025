f5ae6c7f4e7b4c00fc92293b13bc9ccad3054d9d
MalvinaBraucher <malvina.braucher@student-cs.fr>
2026-01-18 12:23:20 +0100

    Modifs par Malvina 18/01

--- main.py
+++ main.py
@@ -12,20 +12,21 @@ from treatmentCSV import*
 path = Path(input("Please enter your path to your file (with / instead of anti-slash and without quotation mark):"))
 
 # Determine if the file is a CSV or a NetCDF
-'''
+
 suffix = path.suffix.lower()
 
 if suffix == ".csv":
     print("It is a CSV")
 elif suffix in (".nc", ".nc4", ".netcdf"):
-    print("it is a NetCDF")
+    print("It is a NetCDF")
 else:
-    print("It is not a CSV nor a NetCDF")
-    '''
+    print("It is not a CSV nor a NetCDF: type not supported yet by this code.")
+    
 
 # CSV case
 df = pd.read_csv(path, sep = ";")
 print(df.info())
+visualisation = float(input("Which kind of visualization do you want? Here are the possible choices, type the number corresponding to your choice: 1 2 3 ..."))
 # Which kind of visualization do you want?
 # en fonction de la réponse demander les colonnes d'intéret :
 # - date, - une ou deux variables autres, plusieurs variables en fonction du temps ?
@@ -33,30 +34,67 @@ print(df.info())
 ### ATTENTIN PBM D'INDENTATION
 
 # cas 1 date 1 variable
-date = input("What is your date column name? mettre les spec de typo")
-column1 = input("What is your column of interest ?")
+if visualisation == 1 or visualisation == 2 :
+    date = input("What is your date column name? mettre les spec de typo")
+    column1 = input("What is your column of interest ?")
+    df[date] = pd.to_datetime(df[date], dayfirst=True)
+    df[column1] = (
+        df[column1]
+        .str.replace(",",".", regex=False)
+        .astype(float)
+    )
 
-'''
-#cas juste une variabel
-column1 = input("What is your column of interest ?")
+#cas juste une variable
+elif visualisation == 3 :
+    column1 = input("What is your column of interest ?")
+    statistic = float(input("Which statistic do you want to compute and display? Here are the possible choices, type the number corresponding to your choice: 1 2 3 ..." ))
+    df[column1] = (
+        df[column1]
+        .str.replace(",",".", regex=False)
+        .astype(float)
+    )
 
 # cas 1 date 2 variables
-date = input("What is your date column name? mettre les spec de typo")
-column1 = input("What is your column 1 of interest ?")
-column2 = input("What is your column 2 of interest ?")
+elif visualisation == 4 or visualisation == 5 : :
+    date = input("What is your date column name? mettre les spec de typo")
+    column1 = input("What is your column 1 of interest ?")
+    column2 = input("What is your column 2 of interest ?")
+    df[date] = pd.to_datetime(df[date], dayfirst=True)
+    df[column1] = (
+        df[column1]
+        .str.replace(",",".", regex=False)
+        .astype(float)
+    )
+    df[column2] = (
+        df[column2]
+        .str.replace(",",".", regex=False)
+        .astype(float)
+    )
 
 # cas 2 variables
-column1 = input("What is your column 1 of interest ?")
-column2 = input("What is your column 2 of interest ?")
-'''
+elif visualisation == 6 :
+    column1 = input("What is your column 1 of interest ?")
+    column2 = input("What is your column 2 of interest ?")
+    statistic_column1 = float(input("Which statistic do you want to compute on your column 1 and display? Here are the possible choices, type the number corresponding to your choice: 1 2 3 ..." ))
+    statistic_column2 = float(input("Which statistic do you want to compute on your column 2 and display? Here are the possible choices, type the number corresponding to your choice: 1 2 3 ..." ))
+    df[column1] = (
+        df[column1]
+        .str.replace(",",".", regex=False)
+        .astype(float)
+    )
+    df[column2] = (
+        df[column2]
+        .str.replace(",",".", regex=False)
+        .astype(float)
+    )
+
 ########
 
-#on convertit les colonnes dans el bon type si ce sont des objets
+#on convertit les colonnes dans le bon type si ce sont des objets
 #il faudra voir comment on gère les types de colonnes
 #mettre un test du type ?
-df[colonne] = (
-    df[colonne]
-    .str.replace(",",".", regex=False)
-    .astype(float)
-)
+
+# Gérer le problème demande d'une courbe temporelle/demande d'une moyenne 
+statistic = float(input("Which statistic do you want to compute and display? Here are the possible choices, type the number corresponding to your choice: 1 2 3 ..." ))
+
 
