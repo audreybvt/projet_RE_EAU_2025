7f9684c04ecf32497cbf1fa0827d85dbc0cee477
MalvinaBraucher <malvina.braucher@student-cs.fr>
2026-01-18 20:02:09 +0100

    Update Malvina 18/01

--- statistics.py
+++ statistics.py
@@ -6,3 +6,14 @@ def maximum_value(X):
 
 def minimum_value(X):
     return X.min()
+
+def percentile(X, q): # When the q_th percentile is needed, 0 < q < 1
+    return X.quantile(q)
+
+def nombre_ocurrences_au_dessus_seuil(X,seuil): # Attention à la définition de X
+    compteur=0
+    for x in X:
+        if x>=seuil :
+            compteur+=1
+    return compteur
+    
