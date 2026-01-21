b528d083a784f79e3e0ab8583a070cd13bb799f9
abovet <audrey.bovet@ensae.fr>
2026-01-21 09:44:13 +0000

    ça a l'air de fonctionner en ajoutant des stat

--- main.py
+++ main.py
@@ -48,6 +48,16 @@ if number_of_variables == 1:
     statistic1 = float(input("Which statistic do you want to compute and display? Here are the possible choices, type the number corresponding to your choice: 1 2 3 ...")) 
     if statistic1 == 1:
         print("The mean is", mean_value(df[column1]))
+    if statistic1 == 2:
+        print("The maximum is",maximum_value(df[column1]))
+    if statistic1 == 3:
+        print("The minimum is",minimum_value(df[column1]))
+    if statistic1 == 4:
+        p = float(input("Which percentile do you want (between 0 and 1 for exemple if you want 5 percent give us 0.05)?"))
+        print("The percentile is",percentile(df[column1],p))
+    if statistic1 == 5:
+        s = float(input("Which threshold do you want?"))
+        print("Number of occurences above the threshold", nombre_occurences_au_dessus_seuil(df[column1],s))
     visualization1 = float(input("Which kind of visualization do you want? Here are the possible choices, type the number corresponding to your choice: 1 2 3 ...")) 
 
 if number_of_variables == 2: 
