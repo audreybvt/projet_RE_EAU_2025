6a192f59517527c685e8051a994f7c5e36a2c54c
MalvinaBraucher <malvina.braucher@student-cs.fr>
2026-03-16 16:37:43 +0100

    stats actualisées

--- data_formatting.py
+++ data_formatting.py
@@ -126,6 +126,7 @@ def csv_to_xarray(filepath):
     # Si colonne date trouvée → index temporel
     if date_col:
         df = df.set_index(date_col)
+        df.index.name = "time"
 
     # Cas long (au moins une colonne catégorielle)
     if object_cols:
